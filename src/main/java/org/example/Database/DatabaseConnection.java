d7910f76da65cb8c9116154e898765b3ec927f4e
Boti <medgyesi.me@icloud.com>
2025-03-14 15:02:14 +0100

    Succesfully made connection to the database (Deleted the password for this commit and push)

--- src/main/java/org/example/Database/DatabaseConnection.java
+++ src/main/java/org/example/Database/DatabaseConnection.java
@@ -1,4 +1,43 @@
 package org.example.Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/Fitness_Tracker_DB";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+
+    // Make connection
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Database connected successfully.");
+        } catch (ClassNotFoundException e) {
+            System.out.println("MYSQL JDBC Driver not found.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Database connection failed.");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    // Close connection
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Connection closed");
+            }
+        } catch (SQLException e) {
+            System.out.println("Connection could not be closed.");
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.example.Database.DatabaseConnection;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Connection connection = DatabaseConnection.getConnection();
+        if (connection != null) {
+            System.out.println("Connected to MySQL!");
+            DatabaseConnection.closeConnection(connection);
+        } else {
+            System.out.println("Failed to connect/:(");
+        }
     }
 }
